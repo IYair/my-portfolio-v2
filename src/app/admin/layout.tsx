b8b34b0b249b73999fdd8dfea7185e459a8788f9
IYair <al046638@uacam.mx>
2025-09-24 23:32:57 -0600

    feat: agregar lógica para omitir AdminLayout en la página de login

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,9 +1,19 @@
+"use client";
+
 import AdminLayout from "@/components/admin/AdminLayout";
+import { usePathname } from "next/navigation";
 
 export default function AdminLayoutWrapper({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const pathname = usePathname();
+  
+  // Si estamos en la página de login, no aplicar AdminLayout
+  if (pathname === "/admin/login") {
+    return <>{children}</>;
+  }
+  
   return <AdminLayout>{children}</AdminLayout>;
 }
